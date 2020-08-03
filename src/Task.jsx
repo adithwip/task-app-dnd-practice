01ee8759dd8323fc96812faff01d85bd36b0bb23
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-08-03 11:06:18 +0700

    implement snapshot and draggableHandle

--- src/Task.jsx
+++ src/Task.jsx
@@ -7,22 +7,37 @@ const Container = styled.div`
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: white;
+
+  transition: background-color 0.2s ease-in-out;
+  background-color: ${props => props.isDragging ? 'lightblue' : 'white'};
+
+  display: flex;
+`;
+
+const Handle = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: orange;
+  border-radius: 4px;
+  margin-right: 8px;
 `;
 
 export default class Task extends React.Component {
   render() {
     return (
       <Draggable draggableId={this.props.task.id} index={this.props.index}>
-        {provided => (
-          <Container
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-            ref={provided.innerRef}
-          >
-            {this.props.task.content}
-          </Container>
-        )}
+        {(provided, snapshot) => {
+          return (
+            <Container
+              {...provided.draggableProps}
+              ref={provided.innerRef}
+              isDragging={snapshot.isDragging}
+            >
+              <Handle {...provided.dragHandleProps} />
+              {this.props.task.content}
+            </Container>
+          )
+        }}
       </Draggable>
     );
   }
